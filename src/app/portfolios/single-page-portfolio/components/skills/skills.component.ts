8b5e6afe444e78980dc062cdbbaf6e480dfe7484
Shahrukh Mansuri <mansurishahrukh007@gmail.com>
2021-01-10 20:01:40 +0530

    Skills Data Entry Done

--- src/app/portfolios/single-page-portfolio/components/skills/skills.component.ts
+++ src/app/portfolios/single-page-portfolio/components/skills/skills.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 
+declare let $: any;
 @Component({
   selector: 'app-skills',
   templateUrl: './skills.component.html',
-  styleUrls: ['./skills.component.scss']
+  styleUrls: ['./skills.component.scss'],
 })
 export class SkillsComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
-  }
+    $('.level-bar-inner').each(function () {
+      var itemWidth = $(this).data('level');
 
+      $(this).animate(
+        {
+          width: itemWidth,
+        },
+        800
+      );
+    });
+  }
 }
