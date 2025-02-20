ea708dc5aafa8926abe8d24c0fa01f0b1606a5db
Sohrab Alam Ansari <ansari.shohrab80@gmail.com>
2025-02-20 14:47:17 +0530

    Checking deployment with actions

--- src/components/about/technologies/Technologies.js
+++ src/components/about/technologies/Technologies.js
@@ -1,15 +1,26 @@
 import UILoader from "../../shared/ui-loader/UILoader";
 import TechnologyItem from "./technology-item/TechnologyItem";
 import "./Technologies.scss";
+import { useEffect, useState } from "react";
 
 const Technologies = (props) => {
   const { technologies } = props;
+  const [sortedTechnologies, setSortedTechnologies] = useState([]);
+
+  useEffect(() => {
+    if (technologies?.length) {
+      setSortedTechnologies(
+        [...technologies].sort((a, b) => a.title.localeCompare(b.title))
+      );
+      console.log(sortedTechnologies);
+    }
+  }, [technologies]);
   return (
     <div className="technologies-container">
       <h3 className="h3 clients-title"> Tech Experiences</h3>
-      {!technologies?.length && <UILoader />}
+      {!sortedTechnologies?.length && <UILoader />}
       <ul className="clients-list has-scrollbar">
-        {technologies?.map((technology, index) => {
+        {sortedTechnologies?.map((technology, index) => {
           return (
             <li key={index}>
               <TechnologyItem technology={technology} />
